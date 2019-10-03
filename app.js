22915d09901da6fe7c37972b72dfc9ddbf49577a
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-10-03 01:39:06 -0500

    accept arguments in to program and framed function blocks for each command

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 // import modules and keys
 require('dotenv').config();
 
-
 const 
 axios = require('axios'),
 moment = require('moment'),
 fs = require('fs'),
-Spotify = require('node-spotify-api');
+Spotify = require('node-spotify-api'),
+[cmd, qus] = process.argv.slice(2);
+
+switch(cmd) {
+
+  case 'concert-this': 
+    console.log(cmd, qus);
+    concertThis();
+    break;
+
+  case 'spotify-this-song':
+    spotifyThisSong();
+    break;
+
+  case 'movie-this':
+    movieThis();
+    break;
+
+  case 'do-what-it-says':
+    doWhatItSays();
+    break;
+
+}
+
   
 
-
-  
 const spotify = new Spotify({
   id: process.env.CLIENT_ID,
   secret: process.env.SECRET
@@ -26,3 +46,15 @@ spotify
   .catch(function(err) {
     console.error('Error occurred: ' + err); 
   });
+function concertThis(qus) {
+
+}
+function spotifyThisSong(qus) {
+
+}
+function movieThis(qus) {
+
+}
+function doWhatItSays(qus) {
+
+}
